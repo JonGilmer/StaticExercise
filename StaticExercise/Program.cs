dc736c15935ba1659695ea74c0b5a21ef39d85ce
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-02-24 10:59:21 -0500

    Pushed completed version of Static Exercise.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double currentTempCelsius = 0;
+            currentTempCelsius = TempConverter.CelsiusToFahrenheit(currentTempCelsius);
+            Console.WriteLine($"{currentTempCelsius} degrees Fahrenheit");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            double currentTempFahrenheit = 32;
+            currentTempFahrenheit = TempConverter.FahrenheitToCelsius(currentTempFahrenheit);
+            Console.WriteLine($"{currentTempFahrenheit} degrees Celsius");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        // Default constructor
+        static TempConverter()
+        {
+        }
+
+        // Converts a temperature in degrees Fahrenheit to a temp in degrees Celsius.
+        public static double FahrenheitToCelsius(double tempFahrenheit)
+        {
+            double toCelsiusTemp = (tempFahrenheit - 32) / 1.8;
+            return toCelsiusTemp;
+        }
+
+        // Converts a temperature in degrees Celsius to a temp in degrees Fahrenheit.
+        public static double CelsiusToFahrenheit(double tempCelsius)
+        {
+            double toFahrenheitTemp = ((9 / 5) * tempCelsius) + 32;
+            return toFahrenheitTemp;
+        }
+    }
+}
